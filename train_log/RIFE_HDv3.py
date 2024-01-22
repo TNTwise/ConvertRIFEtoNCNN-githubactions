dac802bb27e6a1a67a77eab06700216b5e1bbfd3
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-01-22 15:15:49 -0600

    back to rife 4.14

--- train_log/RIFE_HDv3.py
+++ train_log/RIFE_HDv3.py
@@ -18,6 +18,7 @@ class Model:
         self.device()
         self.optimG = AdamW(self.flownet.parameters(), lr=1e-6, weight_decay=1e-4)
         self.epe = EPE()
+        self.version = 4.8
         # self.vgg = VGGPerceptualLoss().to(device)
         self.sobel = SOBEL()
         if local_rank != -1:
@@ -52,11 +53,11 @@ class Model:
         if rank == 0:
             torch.save(self.flownet.state_dict(),'{}/flownet.pkl'.format(path))
 
-    def inference(self, img0, img1, scale=1.0):
+    def inference(self, img0, img1, timestep=0.5, scale=1.0):
         imgs = torch.cat((img0, img1), 1)
-        scale_list = [4/scale, 2/scale, 1/scale]
-        flow, mask, merged = self.flownet(imgs, scale_list)
-        return merged[2]
+        scale_list = [8/scale, 4/scale, 2/scale, 1/scale]
+        flow, mask, merged = self.flownet(imgs, timestep, scale_list)
+        return merged[3]
     
     def update(self, imgs, gt, learning_rate=0, mul=1, training=True, flow_gt=None):
         for param_group in self.optimG.param_groups:
@@ -67,21 +68,21 @@ class Model:
             self.train()
         else:
             self.eval()
-        scale = [4, 2, 1]
+        scale = [8, 4, 2, 1]
         flow, mask, merged = self.flownet(torch.cat((imgs, gt), 1), scale=scale, training=training)
-        loss_l1 = (merged[2] - gt).abs().mean()
-        loss_smooth = self.sobel(flow[2], flow[2]*0).mean()
+        loss_l1 = (merged[3] - gt).abs().mean()
+        loss_smooth = self.sobel(flow[3], flow[3]*0).mean()
         # loss_vgg = self.vgg(merged[2], gt)
         if training:
             self.optimG.zero_grad()
-            loss_G = loss_cons + loss_smooth * 0.1
+            loss_G = loss_l1 + loss_cons + loss_smooth * 0.1
             loss_G.backward()
             self.optimG.step()
         else:
             flow_teacher = flow[2]
-        return merged[2], {
+        return merged[3], {
             'mask': mask,
-            'flow': flow[2][:, :2],
+            'flow': flow[3][:, :2],
             'loss_l1': loss_l1,
             'loss_cons': loss_cons,
             'loss_smooth': loss_smooth,

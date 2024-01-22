6d90e491c2f4500df0e9869eb4fb75fe4d99cd98
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-01-22 13:43:11 -0600

    Update fix_param_file.py

--- fix_param_file.py
+++ fix_param_file.py
@@ -36,16 +36,13 @@ for iteration,line in enumerate(flowparam):
                         line =(line.replace('-23309=1,2 -23310=1,3 -23311=1,0','-23309=1,2 -23310=1,4 -23311=1,0'))  
     if pnnx:
         if 'Crop' in line:
-            if 'splitncnn' in line:
+            
                 if 'pow' in flowparam[iteration+1] or 'warp' in flowparam[iteration+1]:
                     line = line.replace('-23310=1,3 -23311=1,0 -23309=1,0','-23310=1,2 -23311=1,0 -23309=1,0')
-            if 'splitncnn' in line:
                 if 'pow' in flowparam[iteration+1] or 'warp' in flowparam[iteration+1]:
                     line = line.replace('-23310=1,4 -23311=1,0 -23309=1,1','-23310=1,4 -23311=1,0 -23309=1,2')
-            if 'splitncnn' in line:
                 if 'pow' in flowparam[iteration+1] or 'warp' in flowparam[iteration+1]:
                     line = line.replace('-23310=1,2 -23311=1,0 -23309=1,1','-23310=1,2 -23311=1,0 -23309=1,0')
-            if 'splitncnn' in line:
                 if 'pow' in flowparam[iteration+1] or 'warp' in flowparam[iteration+1]:
                     line = line.replace('-23310=1,3 -23311=1,0 -23309=1,2','-23310=1,4 -23311=1,0 -23309=1,2')
         

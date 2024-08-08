738761c82ae99b4aa8dd0d41ff54f28b4ce1e30e
tntwise <tntwise3@gmail.com>
2024-08-08 08:09:37 -0500

    4.22

--- train_log/IFNet_HDv3.py
+++ train_log/IFNet_HDv3.py
@@ -71,7 +71,7 @@ class IFBlock(nn.Module):
             ResConv(c),
         )
         self.lastconv = nn.Sequential(
-            nn.ConvTranspose2d(c, 4*6, 4, 2, 1),
+            nn.ConvTranspose2d(c, 4*13, 4, 2, 1),
             nn.PixelShuffle(2)
         )
 
@@ -86,18 +86,32 @@ class IFBlock(nn.Module):
         tmp = F.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
         flow = tmp[:, :4] * scale
         mask = tmp[:, 4:5]
-        return flow, mask
+        feat = tmp[:, 5:]
+        return flow, mask, feat
         
 class IFNet(nn.Module):
     def __init__(self):
         super(IFNet, self).__init__()
-        self.block0 = IFBlock(7+16, c=192)
-        self.block1 = IFBlock(8+4+16, c=128)
-        self.block2 = IFBlock(8+4+16, c=96)
-        self.block3 = IFBlock(8+4+16, c=64)
-        self.encode = Head()        
-        # self.contextnet = Contextnet()
-        # self.unet = Unet()
+        self.block0 = IFBlock(7+16, c=256)
+        self.block1 = IFBlock(8+4+16+8, c=192)
+        self.block2 = IFBlock(8+4+16+8, c=96)
+        self.block3 = IFBlock(8+4+16+8, c=48)
+        self.encode = Head()
+
+        # not used during inference
+        self.teacher = IFBlock(8+4+16+3+8, c=96)
+        self.caltime = nn.Sequential(
+            nn.Conv2d(16+9, 32, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(32, 64, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(64, 1, 3, 1, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, x, timestep=0.5, scale_list=[8, 4, 2, 1], training=False, fastmode=True, ensemble=False):
         if training == False:
@@ -121,19 +135,15 @@ class IFNet(nn.Module):
         block = [self.block0, self.block1, self.block2, self.block3]
         for i in range(4):
             if flow is None:
-                flow, mask = block[i](torch.cat((img0[:, :3], img1[:, :3], f0, f1, timestep), 1), None, scale=scale_list[i])
+                flow, mask, feat = block[i](torch.cat((img0[:, :3], img1[:, :3], f0, f1, timestep), 1), None, scale=scale_list[i])
                 if ensemble:
-                    f_, m_ = block[i](torch.cat((img1[:, :3], img0[:, :3], f1, f0, 1-timestep), 1), None, scale=scale_list[i])
-                    flow = (flow + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
-                    mask = (mask + (-m_)) / 2
+                    print("warning: ensemble is not supported since RIFEv4.21")
             else:
                 wf0 = warp(f0, flow[:, :2])
                 wf1 = warp(f1, flow[:, 2:4])
-                fd, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], wf0, wf1, timestep, mask), 1), flow, scale=scale_list[i])
+                fd, m0, feat = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], wf0, wf1, timestep, mask, feat), 1), flow, scale=scale_list[i])
                 if ensemble:
-                    f_, m_ = block[i](torch.cat((warped_img1[:, :3], warped_img0[:, :3], wf1, wf0, 1-timestep, -mask), 1), torch.cat((flow[:, 2:4], flow[:, :2]), 1), scale=scale_list[i])
-                    fd = (fd + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
-                    mask = (m0 + (-m_)) / 2
+                    print("warning: ensemble is not supported since RIFEv4.21")
                 else:
                     mask = m0
                 flow = flow + fd
